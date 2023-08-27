fc168bfe32958c3537ef6ef47d7ad6ccf38d3349
Abhinay Kumar <kabhinayak02@gmail.com>
2023-08-28 03:18:18 +0530

    Add logout function

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -5,18 +5,17 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, "Please provide a username"],
         unique: true,
-
     },
     email: {
         type: String,
-        required: [true, "Please provide a username"],
+        required: [true, "Please provide a email"],
         unique: true,
     },
     password: {
         type: String,
-        required: [true, "Please provide a username"],
+        required: [true, "Please provide a password"],
     },
-    isVarified: {
+    isVerfied: {
         type: Boolean,
         default: false,
     },
@@ -30,6 +29,6 @@ const userSchema = new mongoose.Schema({
     verifyTokenExpiry: Date,
 })
 
-const User = mongoose.models.user || mongoose.model('users', userSchema);
+const User = mongoose.models.users || mongoose.model("users", userSchema);
 
 export default User;
